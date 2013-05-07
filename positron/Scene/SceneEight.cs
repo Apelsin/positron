920f44810e452769c44d6bdc1489db9d626f606f
William Pham <ssybrik@hotmail.com>
2013-05-07 14:42:01 -0700

    Scene Seven commit

---
+++ positron/Scene/SceneEight.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+
+namespace positron
+{
+	public class SceneEight : SceneBasicBox
+	{
+		protected SceneEight ():
+			base()
+		{
+		}
+		protected override void InstantiateConnections()
+		{
+			_DoorToPreviousScene = new Door(Rear, 0, 0);
+			//_DoorToNextScene = new Door(Rear, _DoorToPreviousScene.CornerX + 8 * TileSize, 3 * TileSize);
+			_DoorToNextScene = new Door(Rear, _DoorToPreviousScene.CornerX + 3 * TileSize, _DoorToPreviousScene.CornerY);
+		}
+		protected override void InitializeScene ()
+		{
+			// Assign base class variables here, before calling the base class initializer
+			PerimeterOffsetX = 102;
+			PerimeterOffsetY = -100;
+			PerimeterX = 30;
+			PerimeterY = 10;
+			double x0 = PerimeterOffsetX * TileSize;
+			double y0 = PerimeterOffsetY * TileSize;
+			
+			// Setup background tiles
+			var BackgroundTiles = new TileMap (Background, 48, 24, Texture.Get ("sprite_tile_bg2_atlas"));
+			BackgroundTiles.PositionX = (PerimeterOffsetX - 24) * TileSize;
+			BackgroundTiles.PositionY = (PerimeterOffsetY - 4) * TileSize;
+			BackgroundTiles.PositionZ = 1.0;
+			BackgroundTiles.RandomMap ();
+			BackgroundTiles.Build ();
+			
+			// Set up previous door:
+			Scene prev_scene = (Scene)Program.MainGame.Scenes["SceneSeven"];
+			_DoorToPreviousScene.Destination = prev_scene.DoorToNextScene;
+			
+			// Store width and height in local variables for easy access
+			int w_i = (int)ViewWidth;
+			int h_i = (int)ViewHeight;
+			
+			// X and Y positioner variables
+			double xp = TileSize * PerimeterOffsetX;
+			double yp = TileSize * PerimeterOffsetY;
+			
+			xp = x0 + TileSize * 0;
+			yp = y0 + TileSize * 0;
+			
+			new FloorTile (Rear, xp + TileSize * (6 - 1), yp + TileSize * 1);
+			new FloorTile (Rear, xp + TileSize * 6, yp + TileSize * 1);
+			new FloorTile (Rear, xp + TileSize * 6, yp + TileSize * 2);
+			new FloorTile (Rear, xp + TileSize * 6, yp + TileSize * 3);
+			new FloorTile (Rear, xp + TileSize * (6 + 1), yp + TileSize * 1);
+			new FloorTile (Rear, xp + TileSize * (6 + 1), yp + TileSize * 2);
+			new FloorTile (Rear, xp + TileSize * (6 + 2), yp + TileSize * 1);
+			
+			//new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 1, false);
+			ExtenderPlatform ep0 = new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 2, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 3, ep0);
+			//new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 4, ep0);
+			
+			var fs0 = new PressureSwitch (Front, xp + TileSize * 5, yp + TileSize * 2, (sender, e) => {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep0.OnAction (e.Self, new ActionEventArgs (bstate, ep0));
+			}, 3.0);
+			
+			
+			
+			ExtenderPlatform ep1 = new ExtenderPlatform (Stage, xp + TileSize * (8), yp + TileSize * 3, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (9), yp + TileSize * 3, ep1);
+			new ExtenderPlatform (Stage, xp + TileSize * (12), yp + TileSize * 3, ep1);
+			new ExtenderPlatform (Stage, xp + TileSize * (13), yp + TileSize * 3, ep1);
+			ExtenderPlatform ep2 = new ExtenderPlatform (Stage, xp + TileSize * (16), yp + TileSize * 4, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (17), yp + TileSize * 4, ep2);
+			//new ExtenderPlatform (Stage, xp + TileSize * (20), yp + TileSize * 5, ep2);
+			ExtenderPlatform ep3 = new ExtenderPlatform (Stage, xp + TileSize * (21), yp + TileSize * 4.5, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (25), yp + TileSize * 4, ep3);
+			new ExtenderPlatform (Stage, xp + TileSize * (26), yp + TileSize * 4, ep3);
+			
+			var fs1 = new PressureSwitch (Front, xp + TileSize * 6, yp + TileSize * 4, (sender, e) => {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep1.OnAction (e.Self, new ActionEventArgs (bstate, ep1));
+			}, 2.0);
+			
+			var fs2 = new PressureSwitch (Front, xp + TileSize * 6, yp + TileSize * 4, (sender, e) => {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep2.OnAction (e.Self, new ActionEventArgs (bstate, ep2));
+			}, 3.0);
+			
+			var fs3 = new PressureSwitch (Front, xp + TileSize * 6, yp + TileSize * 4, (sender, e) => {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep3.OnAction (e.Self, new ActionEventArgs (bstate, ep3));
+			}, 4.5);
+			
+			//new FloorTile (Rear, xp + TileSize * 25, yp + 6 * TileSize);
+			//new FloorTile (Rear, xp + TileSize * 26, yp + 6 * TileSize);
+			new FloorTile (Rear, xp + TileSize * 27, yp + 4 * TileSize);
+			var ft_door = new FloorTile (Rear, xp + TileSize * 28, yp + 4 * TileSize);
+			new FloorTile (Rear, xp + TileSize * 29, yp + 4 * TileSize);
+			
+			Scene next_scene = (Scene)Program.MainGame.Scenes["SceneEight"];
+			_DoorToNextScene.CornerX = ft_door.CornerX;
+			_DoorToNextScene.CornerY = ft_door.CornerY + TileSize;
+			_DoorToNextScene.Destination = next_scene.DoorToPreviousScene;
+			_DoorToNextScene.Destination.Corner += _DoorToNextScene.Corner;
+			
+			
+			
+			// Call the base class initializer
+			base.InitializeScene ();
+		}
+	}
+}
+
--- positron/Scene/SceneSeven.cs
+++ positron/Scene/SceneSeven.cs
@@ -24,36 +24,19 @@ namespace positron
 		}
 		protected override void InstantiateConnections()
 		{
-			_DoorToPreviousScene = new Door(Rear, 0, -30);
+			_DoorToPreviousScene = new Door(Rear, 0, 0);
 			//_DoorToNextScene = new Door(Rear, _DoorToPreviousScene.CornerX + 8 * TileSize, 3 * TileSize);
 			_DoorToNextScene = new Door(Rear, _DoorToPreviousScene.CornerX + 3 * TileSize, _DoorToPreviousScene.CornerY);
 		}
 		protected override void InitializeScene ()
 		{
 			// Assign base class variables here, before calling the base class initializer
+			PerimeterOffsetX = 125;
+			PerimeterOffsetY = -96;
+			PerimeterX = 7;
+			PerimeterY = 30;
 			double x0 = PerimeterOffsetX * TileSize;
 			double y0 = PerimeterOffsetY * TileSize;
-			//PerimeterOffsetX = 100;
-			//PerimeterOffsetY = -8;
-			PerimeterOffsetX = 100;
-			PerimeterOffsetY = -100;
-			PerimeterX = 8;
-			PerimeterY = 40;
-			
-			// Store width and height in local variables for easy access
-			int w_i = (int)ViewWidth;
-			int h_i = (int)ViewHeight;
-			
-			// X and Y positioner variables
-			double xp = TileSize * PerimeterOffsetX;
-			double yp = TileSize * PerimeterOffsetY;
-			
-			xp += TileSize * PerimeterX - (TileSize * 1);
-			yp -= TileSize * 3;
-			
-			// Set up previous door:
-			Scene prev_scene = (Scene)Program.MainGame.Scenes["SceneSix"];
-			_DoorToPreviousScene.Destination = prev_scene.DoorToNextScene;
 			
 			// Setup background tiles
 			var BackgroundTiles = new TileMap (Background, 48, 24, Texture.Get ("sprite_tile_bg2_atlas"));
@@ -63,48 +46,127 @@ namespace positron
 			BackgroundTiles.RandomMap ();
 			BackgroundTiles.Build ();
 			
-			// Control key indicators (info graphics)
-			var f_infogfx = new SpriteBase(Rear, xp - 2 * TileSize, yp, Texture.Get("sprite_infogfx_key_f"));
+			// Set up previous door:
+			Scene prev_scene = (Scene)Program.MainGame.Scenes["SceneSix"];
+			_DoorToPreviousScene.Destination = prev_scene.DoorToNextScene;
 			
+			// Store width and height in local variables for easy access
+			int w_i = (int)ViewWidth;
+			int h_i = (int)ViewHeight;
 			
-			ExtenderPlatform last_platform_0 = new ExtenderPlatform (Stage, xp + TileSize * (6), yp + TileSize * 4, true);
-			new ExtenderPlatform (Stage, xp + TileSize * (7), yp + TileSize * 4, last_platform_0);
+			// X and Y positioner variables
+			double xp = TileSize * PerimeterOffsetX;
+			double yp = TileSize * PerimeterOffsetY;
 			
-			var bs_lower0 = new ProjectileSwitch (Front, xp + TileSize * (16.5), yp + TileSize * (11 + 0.5) , (sender, e) =>
+			xp = x0 + TileSize * 0;
+			yp = y0 + TileSize * 0;
+			
+			ExtenderPlatform ep0 = new ExtenderPlatform (Stage, xp + TileSize * (0), yp + TileSize * 1, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (1), yp + TileSize * 1, ep0);
+			new ExtenderPlatform (Stage, xp + TileSize * (3), yp + TileSize * 2, ep0);
+			new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 3, ep0);
+			new ExtenderPlatform (Stage, xp + TileSize * (6), yp + TileSize * 3, ep0);
+			new ExtenderPlatform (Stage, xp + TileSize * (3), yp + TileSize * 4, ep0);
+
+			new ExtenderPlatform (Stage, xp + TileSize * (0), yp + TileSize * 3, true);
+			new ExtenderPlatform (Stage, xp + TileSize * (1), yp + TileSize * 3, true);
+
+			new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 5, ep0);
+			new ExtenderPlatform (Stage, xp + TileSize * (6), yp + TileSize * 5, ep0);
+			new ExtenderPlatform (Stage, xp + TileSize * (3), yp + TileSize * 6, ep0);
+
+			new FloorTile (Rear, xp + TileSize * (0), yp + TileSize * 7);
+			new FloorTile (Rear, xp + TileSize * (0), yp + TileSize * 8);
+			new FloorTile (Rear, xp + TileSize * (0), yp + TileSize * 9);
+			new FloorTile (Rear, xp + TileSize * (0), yp + TileSize * 10);
+			new FloorTile (Rear, xp + TileSize * (0), yp + TileSize * 11);
+			new FloorTile (Rear, xp + TileSize * (1), yp + TileSize * 7);
+
+			new ExtenderPlatform (Stage, xp + TileSize * (1), yp + TileSize * 8, true);
+			new ExtenderPlatform (Stage, xp + TileSize * (1), yp + TileSize * 9, true);
+
+			new FloorTile (Rear, xp + TileSize * (2), yp + TileSize * 10);
+			new FloorTile (Rear, xp + TileSize * (3), yp + TileSize * 10);
+			new FloorTile (Rear, xp + TileSize * (4), yp + TileSize * 10);
+			new FloorTile (Rear, xp + TileSize * (5), yp + TileSize * 10);
+			new FloorTile (Rear, xp + TileSize * (6), yp + TileSize * 10);
+
+			var ps0 = new ProjectileSwitch (Front, xp + TileSize * (6.25), yp + TileSize * (1.25) , (sender, e) =>
 			                                      {
 				bool bstate = (SwitchState)e.Info != SwitchState.Open;
-				last_platform_0.OnAction (e.Self, new ActionEventArgs (bstate, last_platform_0));
-				//bool bstate_bs_lower_0 = last_platform_2.State != SwitchState.Open;
-				//gw_lower1.OnAction (e.Self, new ActionEventArgs (bstate || bstate_bs_lower_0, gw_lower1));
-			}, 0.1);
-			
-			// Platform set below top
-			ExtenderPlatform last_platform_1 = new ExtenderPlatform (Stage, xp + TileSize * (0), yp + TileSize * 8, true);
-			new ExtenderPlatform (Stage, xp + TileSize * (1), yp + TileSize * 8, last_platform_1);
-			
-			var bs_lower1 = new ProjectileSwitch (Front, xp + TileSize * (22.5), yp + TileSize * (7 + 0.5) , (sender, e) =>
-			                                      {
+				ep0.OnAction (e.Self, new ActionEventArgs (bstate, ep0));
+			}, 2.5);
+
+			ExtenderPlatform ep1 = new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 11, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (6), yp + TileSize * 11, ep1);
+
+			var ps1 = new ProjectileSwitch (Front, xp + TileSize * (0.7), yp + TileSize * (11.5) , (sender, e) =>
+			                                {
 				bool bstate = (SwitchState)e.Info != SwitchState.Open;
-				last_platform_1.OnAction (e.Self, new ActionEventArgs (bstate, last_platform_1));
-				//bool bstate_bs_lower_0 = last_platform_2.State != SwitchState.Open;
-				//gw_lower1.OnAction (e.Self, new ActionEventArgs (bstate || bstate_bs_lower_0, gw_lower1));
-			}, 0.1);
-			
-			double recess_switch = -4.0;
-			double recess_gw = -2.0;
-			
-			var i = 8;
-			
-			new FloorTile (Rear, xp - TileSize * 1, yp + 2 * TileSize + 8 + i);
-			new FloorTile (Rear, xp - TileSize * 0, yp + 2 * TileSize + 8 + i);
-			var ft_door = new FloorTile (Rear, xp + TileSize * 1, yp + 2 * TileSize + 8 + i);
-			new FloorTile (Rear, xp + TileSize * 2, yp + 2 * TileSize + 8 + i);
-			//new FloorTile (Rear, xp + TileSize, yp + 2 * TileSize + 8);
-			
-			//Scene next_scene = (Scene)Program.MainGame.Scenes["SceneSeven"];
-			//_DoorToNextScene = new Door(Rear, x0 + TileSize * 76, y1 + TileSize, next_scene.DoorToPreviousScene);
-			//_DoorToNextScene = new Door(Rear, ft_door.CornerX, ft_door.CornerY + TileSize, next_scene.DoorToPreviousScene);
-			//_DoorToNextScene.Destination.Position += _DoorToNextScene.Position;
+				ep1.OnAction (e.Self, new ActionEventArgs (bstate, ep1));
+			}, 1.0);
+
+			ExtenderPlatform ep2 = new ExtenderPlatform (Stage, xp + TileSize * (3), yp + TileSize * 12, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (0), yp + TileSize * 13, ep2);
+			new ExtenderPlatform (Stage, xp + TileSize * (1), yp + TileSize * 13, ep2);
+
+			var ps2 = new ProjectileSwitch (Front, xp + TileSize * (0.7), yp + TileSize * (11.5) , (sender, e) =>
+			                                {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep2.OnAction (e.Self, new ActionEventArgs (bstate, ep2));
+			}, 2.5);
+
+			ExtenderPlatform ep3 = new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 13, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (6), yp + TileSize * 13, ep3);
+
+			var ps3 = new ProjectileSwitch (Front, xp + TileSize * (0.7), yp + TileSize * (11.5) , (sender, e) =>
+			                                {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep3.OnAction (e.Self, new ActionEventArgs (bstate, ep3));
+			}, 7.5);
+
+			ExtenderPlatform ep4 = new ExtenderPlatform (Stage, xp + TileSize * (0), yp + TileSize * 15, false);
+			new ExtenderPlatform (Stage, xp + TileSize * (1), yp + TileSize * 15, ep4);
+			new ExtenderPlatform (Stage, xp + TileSize * (3), yp + TileSize * 14, ep4);
+			new ExtenderPlatform (Stage, xp + TileSize * (5), yp + TileSize * 15, ep4);
+			new ExtenderPlatform (Stage, xp + TileSize * (6), yp + TileSize * 15, ep4);
+
+			var ps4 = new ProjectileSwitch (Front, xp + TileSize * (0.7), yp + TileSize * (11.5) , (sender, e) =>
+			                                {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep4.OnAction (e.Self, new ActionEventArgs (bstate, ep4));
+			}, 2.0);
+
+			ExtenderPlatform ep5 = new ExtenderPlatform (Stage, xp + TileSize * (3), yp + TileSize * 16, false);
+
+			var ps5 = new ProjectileSwitch (Front, xp + TileSize * (0.7), yp + TileSize * (11.5) , (sender, e) =>
+			                                {
+				bool bstate = (SwitchState)e.Info != SwitchState.Open;
+				ep5.OnAction (e.Self, new ActionEventArgs (bstate, ep5));
+			}, 3.0);
+
+			ps1.Theta += MathHelper.Pi;
+			ps2.Theta += MathHelper.Pi;
+			ps3.Theta += MathHelper.Pi;
+			ps4.Theta += MathHelper.Pi;
+			ps5.Theta += MathHelper.Pi;
+
+
+			FloorTile ft_door = new FloorTile (Rear, xp + TileSize * (3), yp + 8 + TileSize * 20);
+			new ExtenderPlatform (Stage, xp + TileSize * (2), yp + TileSize * 17, true);
+			new ExtenderPlatform (Stage, xp + TileSize * (2), yp + TileSize * 18, true);
+			new ExtenderPlatform (Stage, xp + TileSize * (2), yp + TileSize * 19, true);
+			new ExtenderPlatform (Stage, xp + TileSize * (4), yp + TileSize * 17, true);
+			new ExtenderPlatform (Stage, xp + TileSize * (4), yp + TileSize * 18, true);
+			new ExtenderPlatform (Stage, xp + TileSize * (4), yp + TileSize * 19, true);
+
+
+
+			Scene next_scene = (Scene)Program.MainGame.Scenes["SceneEight"];
+			_DoorToNextScene.CornerX = ft_door.CornerX;
+			_DoorToNextScene.CornerY = ft_door.CornerY + TileSize;
+			_DoorToNextScene.Destination = next_scene.DoorToPreviousScene;
+			_DoorToNextScene.Destination.Corner += _DoorToNextScene.Corner;
 			
 			
 			
