8c3f6f962de8bdc3b8ce2c073a33f0e9f3b021f9
Apelsin <appelsindrikken@gmail.com>
2013-05-08 23:57:13 -0700

    Wow I got flustered

--- positron/Scene/SceneWin.cs
+++ positron/Scene/SceneWin.cs
@@ -7,8 +7,8 @@ namespace positron
 {
     public class SceneWin: Scene
     {
-        Stopwatch derp_timer = new Stopwatch();
-        SpriteBase win_thing;
+        Stopwatch DerpTimer = new Stopwatch();
+        SpriteBase WinThing;
         public SceneWin ():
             base()
         {
@@ -16,23 +16,23 @@ namespace positron
                 if(Program.MainGame.Player1 != null)
                 {
                     Program.MainGame.Player1.Derez();
-					derp_timer.Start();
+					DerpTimer.Start();
                 }
             };
         }
         protected override void InitializeScene()
         {
-            win_thing = new SpriteBase(HUD, ViewWidth / 2.0, ViewHeight / 2.0, Texture.Get ("sprite_win")).CenterShift();
+            WinThing = new SpriteBase(HUD, ViewWidth / 2.0, ViewHeight / 2.0, Texture.Get ("sprite_win")).CenterShift();
             base.InitializeScene();
         }
         public override void Update(double time)
         {
-            double fx = 2 - Math.Cos(MathHelper.TwoPi * derp_timer.Elapsed.TotalSeconds);
-            double gx = 5 * MathHelper.Pi * Math.Sin(MathHelper.Pi * derp_timer.Elapsed.TotalSeconds);
-            win_thing.Scale = new Vector3d(fx);
-            win_thing.Theta = gx;
+            double fx = 2 - Math.Cos(MathHelper.TwoPi * DerpTimer.Elapsed.TotalSeconds);
+            double gx = 5 * MathHelper.Pi * Math.Sin(MathHelper.Pi * DerpTimer.Elapsed.TotalSeconds);
+            WinThing.Scale = new Vector3d(fx);
+            WinThing.Theta = gx;
             base.Update(time);
-			if (derp_timer.Elapsed.TotalSeconds > 5.0) {
+			if (DerpTimer.Elapsed.TotalSeconds > 5.0) {
 				lock(Program.MainUpdateLock)
 					Scene.InstantiateScenes (ref Program.MainGame);
 			}
